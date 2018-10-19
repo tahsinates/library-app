a427742b085f229afb03a4484cf16ea9374bf7b1
Tahsin Ates <>
2018-10-19 14:29:35 +0100

    added route links

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
 
+
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -15,8 +16,23 @@ app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist'))
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
+const nav = [
+  { link: '/books', title: 'Book' },
+  { link: '/authors', title: 'Author' }
+];
+
+const bookRouter = require('./src/routes/bookRoutes')(nav);
+
+app.use('/books', bookRouter);
 app.get('/', (req, res) => {
-  res.render('index', { list: ['a', 'b'], title: 'Library' });
+  res.render(
+    'index',
+    {
+      nav: [{ link: '/books', title: 'Books' },
+      { link: '/authors', title: 'Authors' }],
+      title: 'Library'
+    }
+  );
 });
 
 app.listen(port, () => {
