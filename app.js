ebd432c81f69525066097393835f4c925b3bf1f5
Tahsin Ates <>
2018-10-19 13:50:52 +0100

    added bootstrap files with ejs webpage template

--- app.js
+++ app.js
@@ -4,21 +4,21 @@ const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
 
-const port = process.env.PORT || 3000;
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(morgan('tiny'));
-app.use(express.static(path.join(__dirname, './public')));
+app.use(express.static(path.join(__dirname, '/public/')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './views/index.html'));
+  res.render('index', { list: ['a', 'b'], title: 'Library' });
 });
 
-app.listen(port, (err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    // express.open('http://localhost:' + port);
-    debug(`listening on port ${chalk.green(port)}`);
-  }
+app.listen(port, () => {
+  debug(`listening on port ${chalk.green(port)}`);
 });
