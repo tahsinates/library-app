6bfae8dd9551fca4f6a0cc5f62de035b5cded400
Tahsin Ates <>
2018-10-19 12:58:45 +0100

    added some npm scripts and eslint, nodemon packages

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
-var express = require('express');
-var chalk = require('chalk');
-var debug = require('debug')('app');
-var morgan = require('morgan');
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
 
-var app = express();
+const port = process.env.PORT || 3000;
+const app = express();
 
 app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, './public')));
 
-app.get('/', function(req, res){
-    res.send('Hello from my library app');
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './views/index.html'));
 });
 
-app.listen(3000, function(){
-    debug(`listening on port ${chalk.green('3000')}`);
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    // express.open('http://localhost:' + port);
+    debug(`listening on port ${chalk.green(port)}`);
+  }
 });
